34b27b06eb99f56cba215172d87a54d173ca7721
BoGyeong1 <wls6590@gmail.com>
2023-01-19 09:35:48 +0900

    Up commonCodeOur

--- src/main/java/com/study/study_springboots/dao/CommonCodeOurDao.java
+++ src/main/java/com/study/study_springboots/dao/CommonCodeOurDao.java
@@ -8,14 +8,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
+@Repository
 @Component
 public class CommonCodeOurDao {
     @Autowired
     private SqlSessionTemplate sqlSessionTemplate;
 
     public Object getList(String sqlMapId, Object dataMap) {
-
         Object result = sqlSessionTemplate.selectList(sqlMapId, dataMap);
+        return result;
+    }
+
+    public Object getOne(String sqlMapId, Object dataMap) {
+        Object result = sqlSessionTemplate.selectOne(sqlMapId, dataMap);
+        return result;
+    }
+
+    // 갯수로 리턴된다 (update delete)
+    public Object update(String sqlMapId, Object dataMap) {
+        Object result = sqlSessionTemplate.update(sqlMapId, dataMap);
+        return result;
+    }
+
+    public Object deleteOne(String sqlMapId, Object dataMap) {
+        Object result = sqlSessionTemplate.delete(sqlMapId, dataMap);
         return result;
     }
 
--- src/main/java/com/study/study_springboots/service/CommonCodeOurService.java
+++ src/main/java/com/study/study_springboots/service/CommonCodeOurService.java
@@ -7,12 +7,31 @@ import com.study.study_springboots.dao.CommonCodeOurDao;
 
 @Service
 public class CommonCodeOurService {
+
     @Autowired
     CommonCodeOurDao commonCodeOurDao;
 
     public Object getList(Object dataMap) {
         String sqlMapId = "CommonCodeOur.selectListByUID";
         Object result = commonCodeOurDao.getList(sqlMapId, dataMap);
+        return result;
+    }
+
+    public Object getOne(Object dataMap) {
+        String sqlMapId = "CommonCodeOur.selectByUID";
+        Object result = commonCodeOurDao.getOne(sqlMapId, dataMap);
+        return result;
+    }
+
+    public Object updateOne(Object dataMap) {
+        String sqlMapId = "CommonCodeOur.updateByUID";
+        Object result = commonCodeOurDao.update(sqlMapId, dataMap);
+        return result;
+    }
+
+    public Object deleteOne(Object dataMap) {
+        String sqlMapId = "CommonCodeOur.deleteByUID";
+        Object result = commonCodeOurDao.deleteOne(sqlMapId, dataMap);
         return result;
     }
 }
