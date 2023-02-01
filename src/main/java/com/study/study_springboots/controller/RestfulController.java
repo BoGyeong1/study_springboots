b1f8918788a06421b7a17aee8e4ff2c4fdb815d9
BoGyeong1 <wls6590@gmail.com>
2023-02-01 14:26:51 +0900

    add restfulController

--- src/main/java/com/study/study_springboots/controller/RestfulController.java
+++ src/main/java/com/study/study_springboots/controller/RestfulController.java
@@ -1,15 +1,24 @@
 package com.study.study_springboots.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.study.study_springboots.service.CommonCodeOurService;
+
 import java.util.*;
 
 @RestController
+@CrossOrigin
 public class RestfulController {
+
+    @Autowired
+    CommonCodeOurService commonCodeOurService;
+
     @RequestMapping(value = "/api/v1/helloworld", method = RequestMethod.GET)
     public String helloworld() {
         return "hello world !";
@@ -22,6 +31,19 @@ public class RestfulController {
         result.putAll(params);
         result.put("id", "bogyeongjin");
         return result;
+    }
+
+    // currentPage = 1
+    @RequestMapping(value = "/api/v1/requestParamsWithDB", method = RequestMethod.POST)
+    public Map requestParamsWithDB(@RequestParam Map<String, Object> params) {
+
+        params.put("currentPage", Integer.parseInt((String) params.get("currentPage")));
+        params.put("pageScale", 10);
+
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap = (Map<String, Object>) commonCodeOurService.getListWithPagination(params);
+
+        return resultMap;
     }
 
 }
